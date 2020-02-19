c241d012a69e0f08a5c1b4b63547f2b5994ae722
Илья Осипов <ilaosipov@Ilas-iMac.local>
2020-02-19 22:58:31 +0300

    added object

--- 3-standart-objects/1-object.js
+++ 3-standart-objects/1-object.js
@@ -11,5 +11,44 @@ const user = {
 
 user["sity"] = "Moscow";
 
-console.log(user);
-console.log(user.getInfo());
+const firstName = "Ilya";
+
+const b = { firstName };
+
+const bb = {
+    [firstName]: "Osipov"
+};
+
+const c = {
+    name: "Ilya",
+    surname: "Osipov",
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    },
+    set fullName(n) {
+        console.log(n);
+        const [name, surname] = n.split(" ");
+        Object.assign(this, { name, surname });
+    }
+};
+
+// c.fullName = 1;
+c.fullName = "Katya Chegrincova";
+
+console.log(c);
+console.log(c.fullName);
+// console.log(b);
+// console.log(bb);
+// console.log(user);
+// console.log(user.getInfo());
+
+const { name, surname, fullName } = c;
+
+console.log("name", name);
+console.log("surname", surname);
+console.log("fullName", fullName);
+
+const { name: cName, ...rest } = c;
+
+console.log(cName);
+console.log(rest);
--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-function random(n) {
-    return Math.round(Math.random() * n);
-}
+const out = document.querySelector(".out");
 
-const arr = [];
-
-for (let i = 0; i < 10; i += 1) {
-    arr.push(random(10));
-}
-
-console.log(arr);
-
-const obj = {};
-
-arr.map((item, idx) => {
-    obj.item = item;
-});
-
-console.log(obj);
-
-// TODO  - Функция принимает а вход массив чисел и возвращает объект вида {"число":"количество вхождений"}
-// TODO  - Например arr = [2,2,3,4,5,5,6]  => {"2":2, "3":1, "4":1,"5":2, "6":1}
+out.innerHTML = `<p>${2 + 3}</p>`;
